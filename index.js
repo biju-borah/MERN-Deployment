2781fa3572ed14da7db310463eaa0844a7ecca02
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-06-05 01:16:43 +0530

    added cors

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express")
 const res = require("express/lib/response")
 const app = express()
-
+const cors = require('cors');
 
 // app.use(function (req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //     next();
 // });
+
+app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
